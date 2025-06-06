3c618e2a6e19959e4a94556300773a6e3a34624a
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-06-06 15:42:18 +0800

    Update diy-part2.sh

--- RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part2.sh
+++ RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part2.sh
@@ -31,7 +31,7 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 #./scripts/feeds install -a -f -p qmodem
 
 
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
