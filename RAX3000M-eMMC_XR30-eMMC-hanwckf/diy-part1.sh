e0895d666ee094eb1c88962b05d50d483ece0d65
suixin1314 <56030771+suixin1314@users.noreply.github.com>
2025-07-10 22:30:38 +0800

    Update diy-part1.sh

--- RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
+++ RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
@@ -22,7 +22,7 @@ echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.
 #echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >> feeds.conf.default
 #echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >> feeds.conf.default
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
-echo 'src-git qmodem https://github.com/FUjr/QModem.git' >> feeds.conf.default
+#echo 'src-git qmodem https://github.com/FUjr/QModem.git' >> feeds.conf.default
 
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 #git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
